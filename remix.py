3ccbaac55bb3d9f6d3570fd6f48c047a7708c557
pusp2606-max <pusp2606@gmail.com>
2026-06-09 12:20:41 +0400

    Update remix.py

--- remix.py
+++ remix.py
@@ -1,26 +1,25 @@
 import subprocess
 import os
-
 def make_remix(input_file):
     os.makedirs("output", exist_ok=True)
-
     output_file = "output/remix.mp3"
-
     cmd = [
         "ffmpeg",
         "-y",
         "-i", input_file,
-
-        # Ускорение примерно до hardtekk-темпа
         "-filter:a",
         "asetrate=44100*1.25,aresample=44100,bass=g=8",
-
         "-b:a",
         "320k",
-
         output_file
     ]
-
-    subprocess.run(cmd)
-
+    result = subprocess.run(
+        cmd,
+        capture_output=True,
+        text=True
+    )
+    print(result.stdout)
+    print(result.stderr)
+    if result.returncode != 0:
+        raise Exception("FFmpeg error")
     return output_file
